528495102db6dbc525aba9339b9e74814cd6bacb
riznanasar003 <richurizna10@gmail.com>
2024-06-12 22:05:27 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './Components/Add';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div>
       <Add/>
+      <Search/>
     </div>
   );
 }
